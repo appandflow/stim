4151d431a40a3b290d78f1323dcbee120a4c17c3
Janic Duplessis <janicduplessis@gmail.com>
2026-09-26 01:30:41 -0400

    refactor(mobile): make stim-video an Expo inline module (#1430)
    * refactor(mobile): make stim-video an Expo inline module
    
    * docs(mobile): describe what prebuild wires for the inline module

--- apps/mobile/modules/stim-video/android/StimVideo.kt
+++ apps/mobile/modules/stim-video/android/StimVideo.kt
@@ -4,7 +4,7 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.kotlin.typedarray.Uint8Array
 
-class StimVideoModule : Module() {
+class StimVideo : Module() {
   override fun definition() = ModuleDefinition {
     Name("StimVideo")
 
